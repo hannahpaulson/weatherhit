bc66b1fe2c4ba8bb5f88f6d2f158edd6fd485118
Hannah <hanpaulson@gmail.com>
2018-06-15 12:49:19 +0100

    changed the way that the calls are made so you can search dynamic

--- app/src/main/java/com/hp/weatherhit/APIClient.java
+++ app/src/main/java/com/hp/weatherhit/APIClient.java
@@ -1,14 +1,26 @@
 package com.hp.weatherhit;
 
+import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public class APIClient {
     private static Retrofit retrofit = null;
+    private static final String URL = "http://api.openweathermap.org/";
+
+    public interface APIInterface {
+        @GET("/data/2.5//weather")
+        Call<WeatherData> getLocationWeather(
+                @Query("q") String location,
+                @Query("appId") String appId);
+    }
+
     static Retrofit getClient() {
 
         retrofit = new Retrofit.Builder()
-                .baseUrl("http://api.openweathermap.org/")
+                .baseUrl(URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
--- app/src/main/java/com/hp/weatherhit/MainActivity.java
+++ app/src/main/java/com/hp/weatherhit/MainActivity.java
@@ -5,12 +5,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-    APIInterface apiInterface;
+    APIClient.APIInterface apiInterface;
     WeatherData weatherData;
 
     @Override
@@ -19,14 +20,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final TextView textView = findViewById(R.id.hannah);
 
-        apiInterface = APIClient.getClient().create(APIInterface.class);
-        Call<WeatherData> call = apiInterface.getLocationWeather();
+        apiInterface = APIClient.getClient().create(APIClient.APIInterface.class);
+        Call<WeatherData> call = apiInterface.getLocationWeather("London", "96d06b04ae1e61a7d850e288a8f16b2d");
         call.enqueue(new Callback<WeatherData>() {
             @Override
             public void onResponse(Call<WeatherData> call, Response<WeatherData> response) {
-                weatherData = response.body();
-                Log.i("onCreate", weatherData.getName());
-                textView.setText(weatherData.getName());
+                if (weatherData != null) {
+                    weatherData = response.body();
+                    Log.i("onCreate", weatherData.getName());
+                    textView.setText(weatherData.getName());
+                }
+                Log.i("onCreate", "" + response);
             }
 
             @Override
