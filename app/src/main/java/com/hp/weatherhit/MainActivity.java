d4c612f398c6151a21558185906262626528c24b
Hannah <hanpaulson@gmail.com>
2018-06-15 14:03:10 +0100

    user can search for a location

--- app/src/main/java/com/hp/weatherhit/MainActivity.java
+++ app/src/main/java/com/hp/weatherhit/MainActivity.java
@@ -1,10 +1,16 @@
 package com.hp.weatherhit;
 
-
+import com.hp.weatherhit.R;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -20,8 +26,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final TextView textView = findViewById(R.id.hannah);
 
+        final EditText editText = findViewById(R.id.user_input);
+        final Button button = findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("Hannah", editText.getText().toString());
+                if (editText.getText().toString() != "") {
+                    requestForWeatherData(textView, editText.getText().toString());
+                } else {
+                    Toast.makeText(getApplicationContext(), "Pls Type a location", Toast.LENGTH_SHORT).show();
+                }
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+            }
+        });
+    }
+
+    private void requestForWeatherData(final TextView textView, String location) {
         apiInterface = APIClient.getClient().create(APIClient.APIInterface.class);
-        Call<WeatherData> call = apiInterface.getLocationWeather("London", "96d06b04ae1e61a7d850e288a8f16b2d");
+        Call<WeatherData> call = apiInterface.getLocationWeather(location, "96d06b04ae1e61a7d850e288a8f16b2d");
         call.enqueue(new Callback<WeatherData>() {
             @Override
             public void onResponse(Call<WeatherData> call, Response<WeatherData> response) {
