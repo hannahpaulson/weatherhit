844da5e22d97f3a0814e648ff6b59a707d55e4ac
Hannah <hanpaulson@gmail.com>
2018-06-17 15:25:49 +0100

    removing the fragment cleaning up activity

--- app/src/main/java/com/hp/weatherhit/SettingsActivity.java
+++ app/src/main/java/com/hp/weatherhit/SettingsActivity.java
@@ -1,8 +1,11 @@
 package com.hp.weatherhit;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 
 public class SettingsActivity extends PreferenceActivity {
@@ -10,14 +13,10 @@ public class SettingsActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
-    }
+        addPreferencesFromResource(R.xml.pref_general);
 
-    public static class MyPreferenceFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(final Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.pref_general);
-        }
+//        Context context = getApplicationContext();
+//        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(get);
+//        settings.registerOnSharedPreferenceChangeListener(this);
     }
 }
